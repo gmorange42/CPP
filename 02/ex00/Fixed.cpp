081a81a57dc6be9a1ab245981463fc3dcb9f503f
gmorange <gmorange@student.42.fr>
2022-04-01 19:16:03 +0200

    CPP02 ex00 -> main and .cpp/.hpp created

---
+++ 02/ex00/Fixed.cpp
@@ -0,0 +1,20 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+}
+
+Fixed::Fixed(Fixed const& copy)
+{
+	//assigner les attributs
+	return (*this);
+}
+
+Fixed::~Fixed()
+{
+}
+
+Fixed	Fixed::operator=(Fixed const& ref)
+{
+	return (*this);
+}
---
+++ 02/ex00/Fixed.hpp
@@ -0,0 +1,17 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <string>
+
+class	Fixed
+{
+	public:
+		Fixed();
+		Fixed(Fixed& copy);
+		~Fixed();
+		Fidex&	operator=(Fixed const& ref)
+	private:
+};
+
+#endif
