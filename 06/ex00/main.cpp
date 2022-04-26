6ad7df03a73c877c04a1d1ea7516db93ec866a3e
Guillaume MORANGE <gmorange@e3r1p12.clusters.42paris.fr>
2022-04-26 08:10:36 +0000

    g++ don't works

---
+++ 06/ex00/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+//void	check_type(std::string str)
+//{
+//	for (std::string::iterator it=str.begin(); it!= str.end(); it++)
+//	{
+//		std::cout << *it << std::endl;
+//	}
+//}
+
+int	main(int ac, char ** av)
+{
+	return (0);
+}
